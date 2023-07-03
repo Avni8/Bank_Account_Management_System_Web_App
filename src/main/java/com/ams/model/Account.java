64aadb22e45a893745ef0ddeb9b8aa5ac6e082a2
Avni8 <avnighimire2000@gmail.com>
2023-07-03 11:04:09 +0545

    models updated

--- src/main/java/com/ams/model/Account.java
+++ src/main/java/com/ams/model/Account.java
@@ -8,6 +8,109 @@ package com.ams.model;
  *
  * @author avni
  */
-public class Account {
+public class Account implements IModelId {
+    private String acc_id;
+    private String acc_no;
+    private String customerId;
+    private String acc_type;
+    private double interest_rate;
+    private String opened_date;
+    private String matured_date;
+    private double balance;
+    private Customer customer;
+
+    public Account(String acc_id, String acc_no, String customerId, String acc_type, double interest_rate,
+            String opened_date, String matured_date, double balance, Customer customer) {
+        this.acc_id = acc_id;
+        this.acc_no = acc_no;
+        this.customerId = customerId;
+        this.acc_type = acc_type;
+        this.interest_rate = interest_rate;
+        this.opened_date = opened_date;
+        this.matured_date = matured_date;
+        this.balance = balance;
+        this.customer = customer;
+        
+    }
+    
+    @Override
+    public String getId() {
+        return acc_id;
+    }
+
+    @Override
+    public void setId(String acc_id) {
+        this.acc_id = acc_id;
+    }
+
+    public String getAcc_no() {
+        return acc_no;
+    }
+
+    public void setAcc_no(String acc_no) {
+        this.acc_no = acc_no;
+    }
+
+    public String getAcc_type() {
+        return acc_type;
+    }
+    public String getTableName(){
+        return "account";
+    }
+
+    public void setAcc_type(String acc_type) {
+        this.acc_type = acc_type;
+    }
+
+    public double getInterest_rate() {
+        return interest_rate;
+    }
+
+    public void setInterest_rate(double interest_rate) {
+        this.interest_rate = interest_rate;
+    }
+
+    public String getOpened_date() {
+        return opened_date;
+    }
+
+    public void setOpened_date(String opened_date) {
+        this.opened_date = opened_date;
+    }
+
+    public String getMatured_date() {
+        return matured_date;
+    }
+
+    public void setMatured_date(String matured_date) {
+        this.matured_date = matured_date;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    
+    }
+    
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+    
     
 }
--- src/main/java/com/ams/model/Customer.java
+++ src/main/java/com/ams/model/Customer.java
@@ -8,9 +8,110 @@ package com.ams.model;
  *
  * @author avni
  */
-public class Customer {
+public class Customer implements IModelId{
+    
+    private String id;
+    private String name;
+    private String address;
+    private String contact;
+    private String dob;
+    private String email; 
+    private String product_type;
+    private String username;
+    private String password;
+
+    public Customer(String id, String name, String address, String contact, String dob, String email, 
+                    String product_type, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.dob = dob;
+        this.email = email;
+        this.product_type = product_type;
+        this.username = username;
+        this.password = password;
+    }
     
     
+    @Override
+    public String getId() {
+        return id;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public String getContact() {
+        return contact;
+    }
+    
+    public String getDob() {
+        return dob;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getProductType() {
+        return product_type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+     public String getTableName(){
+        return "customer";
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setProduct_type(String product_type) {
+        this.product_type = product_type;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+   
     
     
 }
