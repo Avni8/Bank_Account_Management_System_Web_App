ec8cc8da3f7d3f9021ab9e19a1b529b4fd777e5c
Avni8 <avnighimire2000@gmail.com>
2023-09-25 12:43:37 +0545

    access control in deposit resource

--- src/main/java/com/ams/restresource/DepositResource.java
+++ src/main/java/com/ams/restresource/DepositResource.java
@@ -9,6 +9,8 @@ import com.ams.repository.ClientRepository;
 import com.ams.repository.AccountRepository;
 import com.ams.service.TransactionService;
 import com.ams.model.Client;
+import com.ams.model.UserRole;
+import com.ams.controller.UserBean;
 import com.ams.request.AccountRequest;
 import com.ams.request.TransactionRequest;
 import javax.ws.rs.POST;
@@ -16,11 +18,14 @@ import javax.ws.rs.Path;
 import com.ams.service.TransactionService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -43,37 +48,64 @@ public class DepositResource {
     @Inject
     AccountRepository accountRepository;
 
+    @Inject
+    @Context
+    HttpServletRequest httpServletRequest;
+
+    @Inject
+    UserBean userBean;
+
     @POST
     public Response deposit(TransactionRequest depositRequest) {
 
+        boolean allow = false;
         Long userId = depositRequest.getUserId();
         List<AccountRequest> accountRequests = depositRequest.getAccountList();
 
         Client selectedUser = userRepository.findById(userId);
-        List<Account> accountList = new ArrayList<>();
 
-        for (AccountRequest accountRequest : accountRequests) {
-            String accountNo = accountRequest.getAccountNo();
-            Account account = accountRepository.findByAccNo(accountNo);
+        UserRole userRole = (UserRole) httpServletRequest.getSession().getAttribute("userRole");
 
-            if (account != null) {
-                account.setAmount(accountRequest.getAmount());
-                accountList.add(account);
-            }
-            else{
-                return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Account not found")
-                    .build();
-            }
+        if (userRole.equals(UserRole.CLIENT)) {
+
+            Client loggedInClient = userBean.getCurrentClient();
+
+            allow = Objects.equals(selectedUser, loggedInClient);
+
         }
 
-        boolean depositSuccessful = transactionService.performDeposit(selectedUser, accountList);
+        if (userRole.equals(UserRole.STAFF) || allow == true) {
 
-        if (depositSuccessful) {
-            return Response.ok("Deposit successful").build();
+            List<Account> accountList = new ArrayList<>();
+
+            for (AccountRequest accountRequest : accountRequests) {
+                String accountNo = accountRequest.getAccountNo();
+                Account account = accountRepository.findByAccNo(accountNo);
+
+                if (account != null) {
+                    account.setAmount(accountRequest.getAmount());
+                    accountList.add(account);
+                } else {
+                    return Response.status(Response.Status.BAD_REQUEST)
+                            .entity("Account not found")
+                            .build();
+                }
+            }
+
+            boolean depositSuccessful = transactionService.performDeposit(selectedUser, accountList);
+
+            if (depositSuccessful) {
+                return Response.ok("Deposit successful").build();
+            } else {
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity("Deposit failed")
+                        .build();
+            }
+
         } else {
+
             return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Deposit failed")
+                    .entity("Access Denied")
                     .build();
         }
     }
